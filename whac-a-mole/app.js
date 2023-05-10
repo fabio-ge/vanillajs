0bff8e6ad27f629e45ad2eddf6dede30c262288e
unknown <fabio_perelli@yahoo.it>
2023-05-10 10:57:31 +0100

    whack a mole: level of difficulty

--- whac-a-mole/app.js
+++ whac-a-mole/app.js
@@ -1,22 +1,49 @@
+//DOM elements
 const moles = document.querySelectorAll('.hole .mole');
+const gameBtns = document.querySelectorAll('.container p button');
+const punti = document.getElementById('punti');
+
+//CONSTANTS
+const DURATION = 20000;
+const BASIC_END_OF_MOLE = 1500;
+const ADV_END_OF_MOLE = 1000;
+
+//State
 let score = 0;
 let isFinish = false;
 let lastMole = null;
-const DURATION = 20000;
+let maxTime = BASIC_END_OF_MOLE;
 
-//event listener sulla talpa per controllare se è stata beccata
+//EVENTS
+gameBtns.forEach(el => {
+    el.addEventListener('click',(e)=>{
+        let level = e.target.value;
+        if(level === 'basic') maxTime = BASIC_END_OF_MOLE;
+        else maxTime = ADV_END_OF_MOLE;
+        play();
+    })
+});
+
+moles.forEach(e => e.addEventListener('click',(ev)=>{
+    score++;
+    setPoints();
+    ev.target.style.height = '0px';
+}));
 
 
+//FUNCTIONS
 function pickRandomMole(){
     return moles[Math.floor(Math.random()*moles.length)];
 }
 
 function randTime(){
     let start = 200;
-    let end = 2000;
-    return Math.floor(Math.random()*(end-start))+start;
+    return Math.floor(Math.random()*(maxTime-start))+start;
 }
 
+function setPoints(){
+    punti.textContent = score;
+}
 
 function popUp(){
     if(isFinish)
@@ -24,19 +51,31 @@ function popUp(){
 
     let singleMole = pickRandomMole();
     let randomTime = randTime();
+
     if(singleMole==lastMole){
         popUp();
+        return;
     }
+    lastMole = singleMole;
     singleMole.style.height = '140px';
     setTimeout(() =>{
         singleMole.style.height = '0px';
-        popUp();
+        setTimeout(()=>{
+            popUp();
+        },300);
+        
     },randomTime);
 }
 
+function reset(){
+    score = 0;
+    setPoints();
+    isFinish = false;
+    lastMole = null;
+}
+
 function play(){
+    reset();
     setTimeout(() => isFinish = true,DURATION);
     popUp();
 }
-
-play();
